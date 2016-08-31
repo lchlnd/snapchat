7654d1268e9a839ac37ab537c239d41415aea751
remkl <remi.kl@gmail.com>
2016-08-31 10:33:55 +1000

    Cleaned up login comp more

--- SnapChat/App/Components/Login/Login.js
+++ SnapChat/App/Components/Login/Login.js
@@ -10,51 +10,15 @@ const Login = ({
   hasValidInput
 }) => (
   <View style={styles.container}>
-    <View style={{backgroundColor: 'transparent'}}>
-      <TouchableHighlight
-        style={styles.backArrowContainer}
-        onPress={backButtonPressed}
-        underlayColor="#F5F5F5"
-      >
-        <Image
-          style={styles.backArrow}
-          source={require('../../../images/back_arrow.png')}
-        />
-      </TouchableHighlight>
-    </View>
+    { backArrow(backButtonPressed) }
 
     <View style={styles.loginHeaderWrapper}>
       <Text style={styles.loginHeader}>Log In</Text>
     </View>
 
     <View style={styles.formContainer}>
-      <Text style={styles.inputLabel}>Username Or Email</Text>
-      <TextInput
-        style={styles.formInput}
-        onChangeText={updateUsername}
-      />
-
-      <Text style={styles.inputLabel}>Password</Text>
-      <TextInput
-        style={styles.formInput}
-        onChangeText={updatePassword}
-      />
-
-      <TouchableHighlight
-        style={styles.loginButtonContainer}
-        onPress={hasValidInput ? loginButtonPressed : () => {}} // TODO: can prob provide error msg
-        underlayColor="#F5F5F5"
-      >
-        <View style={hasValidInput
-          ? styles.loginButtonActivated
-          : styles.loginButtonDeactivated}>
-          <Text style={hasValidInput
-            ? styles.loginButtonActivatedText
-            : styles.loginButtonDeactivatedText}>
-            Log In
-          </Text>
-        </View>
-      </TouchableHighlight>
+      { inputFields(updateUsername, updatePassword) }
+      { loginButton(hasValidInput, loginButtonPressed) }
     </View>
   </View>
 )
@@ -70,3 +34,59 @@ Login.propTypes = {
 }
 
 export default Login
+
+function backArrow (backButtonPressed) {
+  return (
+    <View style={{backgroundColor: 'transparent'}}>
+      <TouchableHighlight
+        style={styles.backArrowContainer}
+        onPress={backButtonPressed}
+        underlayColor="#F5F5F5"
+      >
+        <Image
+          style={styles.backArrow}
+          source={require('../../../images/back_arrow.png')}
+        />
+      </TouchableHighlight>
+    </View>
+  )
+}
+
+function inputFields (updateUsername, updatePassword) {
+  return (
+    <View>
+      <Text style={styles.inputLabel}>Username Or Email</Text>
+      <TextInput
+        style={styles.formInput}
+        onChangeText={updateUsername}
+      />
+
+      <Text style={styles.inputLabel}>Password</Text>
+      <TextInput
+        style={styles.formInput}
+        onChangeText={updatePassword}
+      />
+    </View>
+  )
+}
+
+function loginButton (hasValidInput, loginButtonPressed) {
+  return (
+    <TouchableHighlight
+      style={styles.loginButtonContainer}
+      onPress={hasValidInput ? loginButtonPressed : () => {}} // TODO: can prob provide error msg
+      underlayColor="#F5F5F5"
+    >
+      <View style={hasValidInput
+        ? styles.loginButtonActivated
+        : styles.loginButtonDeactivated}>
+        <Text style={hasValidInput
+          ? styles.loginButtonActivatedText
+          : styles.loginButtonDeactivatedText}>
+          Log In
+        </Text>
+      </View>
+    </TouchableHighlight>
+  )
+}
+
