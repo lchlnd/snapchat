64f83574e32cf655bdf2d4a243328bab0b1dd640
rokane <rokane@student.unimelb.edu.au>
2016-09-02 21:22:51 +1000

    Skeleton & Scrollable view for settings page complete

--- SnapChat/App/Components/Settings/Settings.js
+++ SnapChat/App/Components/Settings/Settings.js
@@ -1,16 +1,23 @@
 import React, { PropTypes } from 'react'
-import { View, Text, TouchableHighlight, Image } from 'react-native'
+import { View, Text, TouchableHighlight, Image, ScrollView } from 'react-native'
 import { settingsStyles as styles } from './settingsStyles'
 
-const Settings = ({backButtonPressed}) => (
+const Settings = ({
+    backButtonPressed,
+    myAccountData,
+    additionalServicesData,
+    whoCanData,
+    moreInformationData,
+    accountActionsData
+    }) => (
 
     <View style={styles.container}>
 
-        {/* Header */}
         <View style={styles.header}>
             <TouchableHighlight
                 onPress={backButtonPressed}
-                underlayColor="#F5F5F5">
+                underlayColor="#F5F5F5"
+            >
                 <Image
                     style={styles.backArrow}
                     source={require('../../../images/back_arrow.png')}
@@ -20,154 +27,46 @@ const Settings = ({backButtonPressed}) => (
             <Text style={styles.backArrow}></Text>
         </View>
 
-        {/*My Account Details*/}
-        <View style={styles.sectionContainer}>
+        <ScrollView>
             <Text style={styles.sectionTitle}>MY ACCOUNT</Text>
-            <View style={styles.listField}>
-                <TouchableHighlight>
-                    <Text>Name</Text>
-                </TouchableHighlight>
-            </View>
-            <View style={styles.listField}>
-                <TouchableHighlight>
-                    <Text>Username</Text>
-                </TouchableHighlight>
-            </View>
-            <View style={styles.listField}>
-                <TouchableHighlight>
-                    <Text>Birthday</Text>
-                </TouchableHighlight>
-            </View>
-            <View style={styles.listField}>
-                <TouchableHighlight>
-                    <Text>Mobile Number</Text>
-                </TouchableHighlight>
-            </View>
-            <View style={styles.listField}>
-                <TouchableHighlight>
-                    <Text>Email</Text>
-                </TouchableHighlight>
-            </View>
-            <View style={styles.listField}>
-                <TouchableHighlight>
-                    <Text>Bitmoji</Text>
-                </TouchableHighlight>
-            </View>
-            <View style={styles.listField}>
-                <TouchableHighlight>
-                    <Text>Password</Text>
-                </TouchableHighlight>
-            </View>
-            <View style={styles.listField}>
-                <TouchableHighlight>
-                    <Text>Login Verification</Text>
-                </TouchableHighlight>
-            </View>
-            <View style={styles.listField}>
-                <TouchableHighlight>
-                    <Text>Notifications</Text>
-                </TouchableHighlight>
-            </View>
-            <View style={styles.listField}>
-                <TouchableHighlight>
-                    <Text>Memories</Text>
-                </TouchableHighlight>
-            </View>
-        </View>
-
-        {/* Additional Services Details */}
-        <View style={styles.sectionContainer}>
+            {myAccountData.map((e, i) => createSectionRow(e, i))}
             <Text style={styles.sectionTitle}>ADDITIONAL SERVICES</Text>
-            <View style={styles.listField}>
-                <TouchableHighlight>
-                    <Text>Manage</Text>
-                </TouchableHighlight>
-            </View>
-        </View>
-
-        {/* Who Can Details */}
-        <View style={styles.sectionContainer}>
-            <Text style={styles.sectionTitle}>WHO CAN...</Text>
-            <View style={styles.listField}>
-                <TouchableHighlight>
-                    <Text>Contact me</Text>
-                </TouchableHighlight>
-            </View>
-            <View style={styles.listField}>
-                <TouchableHighlight>
-                    <Text>View My Story</Text>
-                </TouchableHighlight>
-            </View>
-            <View style={styles.listField}>
-                <TouchableHighlight>
-                    <Text>See me in Quick Add</Text>
-                </TouchableHighlight>
-            </View>
-        </View>
-
-        {/* More Information Details */}
-        <View style={styles.sectionContainer}>
+            {additionalServicesData.map((e, i) => createSectionRow(e, i))}
+            <Text style={styles.sectionTitle}>WHO CAN..</Text>
+            {whoCanData.map((e, i) => createSectionRow(e, i))}
             <Text style={styles.sectionTitle}>MORE INFORMATION</Text>
-            <View style={styles.listField}>
-                <TouchableHighlight>
-                    <Text>Support</Text>
-                </TouchableHighlight>
-            </View>
-            <View style={styles.listField}>
-                <TouchableHighlight>
-                    <Text>Privacy Policy</Text>
-                </TouchableHighlight>
-            </View>
-            <View style={styles.listField}>
-                <TouchableHighlight>
-                    <Text>Terms of Service</Text>
-                </TouchableHighlight>
-            </View>
-            <View style={styles.listField}>
-                <TouchableHighlight>
-                    <Text>Licenses</Text>
-                </TouchableHighlight>
-            </View>
-        </View>
-
-        {/* Account Actions Details */}
-        <View style={styles.sectionContainer}>
+            {moreInformationData.map((e, i) => createSectionRow(e, i))}
             <Text style={styles.sectionTitle}>ACCOUNT ACTIONS</Text>
-            <View style={styles.listField}>
-                <TouchableHighlight>
-                    <Text>Clear cache</Text>
-                </TouchableHighlight>
-            </View>
-            <View style={styles.listField}>
-                <TouchableHighlight>
-                    <Text>Clear Conversations</Text>
-                </TouchableHighlight>
-            </View>
-            <View style={styles.listField}>
-                <TouchableHighlight>
-                    <Text>Restore purchases</Text>
-                </TouchableHighlight>
-            </View>
-            <View style={styles.listField}>
-                <TouchableHighlight>
-                    <Text>Blocked</Text>
-                </TouchableHighlight>
-            </View>
-            <View style={styles.listField}>
-                <TouchableHighlight>
-                    <Text>Logout</Text>
-                </TouchableHighlight>
-            </View>
-        </View>
+            {accountActionsData.map((e, i) => createSectionRow(e, i))}
+        </ScrollView>
 
     </View>
-
 )
+
+const SectionRow = ({field}) => {
+    return (
+        <View style={styles.listField}>
+            <TouchableHighlight>
+                <Text>{field}</Text>
+            </TouchableHighlight>
+        </View>
+    )
+}
+
+const createSectionRow = (settingsObject, i) => {
+    return (
+        <SectionRow
+            key={i}
+            field={settingsObject.field}
+        />
+    )
+}
 
 const func = PropTypes.func.isRequired
 
 Settings.propTypes = {
-    backButtonPressed: func
+    backButtonPressed: func,
+    settingsData: PropTypes.array
 }
 
 export default Settings
--- SnapChat/App/Components/Settings/SettingsContainer.js
+++ SnapChat/App/Components/Settings/SettingsContainer.js
@@ -14,9 +14,52 @@ class SettingsContainer extends Component {
 
     render () {
         return <Settings
+            myAccountData={myAccountData}
+            additionalServicesData={additionalServicesData}
+            whoCanData={whoCanData}
+            moreInformationData={moreInformationData}
+            accountActionsData={accountActionsData}
             backButtonPressed={this.backButtonPressed}
         />
     }
 }
 
 export default SettingsContainer
+
+const myAccountData = [
+    {field: 'Name'},
+    {field: 'Username'},
+    {field: 'Birthday'},
+    {field: 'Mobile Number'},
+    {field: 'Email'},
+    {field: 'Bitmoji'},
+    {field: 'Password'},
+    {field: 'Login Verification'},
+    {field: 'Notifications'},
+    {field: 'Memories'}
+]
+
+const additionalServicesData = [
+    {field: 'Manage'}
+]
+
+const whoCanData = [
+    {field: 'Contact me'},
+    {field: 'View My Story'},
+    {field: 'See me in Quick Add'}
+]
+
+const moreInformationData = [
+    {field: 'Support'},
+    {field: 'Privacy Policy'},
+    {field: 'Terms of Service'},
+    {field: 'Licences'}
+]
+
+const accountActionsData = [
+    {field: 'Clear cache'},
+    {field: 'Clear Conversations'},
+    {field: 'Restore purchases'},
+    {field: 'Blocked'},
+    {field: 'Logout'}
+]
