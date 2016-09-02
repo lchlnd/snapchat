d4f268fc337e683b199e00f1a3801cd338443f56
Lachlan Dee <dee.lachlan@gmail.com>
2016-09-02 14:43:45 +1000

    Added front/back camera toggle, on/off/auto flash toggle.

--- SnapChat/App/Components/Main/Main.js
+++ SnapChat/App/Components/Main/Main.js
@@ -7,18 +7,48 @@ class Main extends Component {
   constructor (props) {
     super()
     this.state = {
-      showProgress: false
+      showProgress: false,
+      frontCamera: false,
+      flash: 'auto'
     }
+    this.handleCameraChange = this.handleCameraChange.bind(this);
+    this.handleFlashChange = this.handleFlashChange.bind(this);
   }
+
+  handleCameraChange() {
+    this.setState({ frontCamera: !this.state.frontCamera})
+  }
+
+  handleFlashChange() {
+    var lookup = {"auto":"on", "on":"off", "off":"auto"}
+    this.setState({ flash: lookup[this.state.flash]})
+  }
+
   render () {
+    const cameraText = this.state.frontCamera ? 'front' : 'back';
+    const flashText = this.state.flash
     return (
       <View style={styles.container}>
         <Camera ref={(cam) => {
           this.camera = cam
         }}
           style={styles.preview}
-          aspect={Camera.constants.Aspect.fill}>
-          <Text style={styles.capture} onPress={this.takePicture.bind(this)}>[CAPTURE]</Text>
+          aspect={Camera.constants.Aspect.fill}
+          type={this.state.frontCamera ? Camera.constants.Type.front : Camera.constants.Type.back}
+          flashMode={this.state.flash == "auto" ? Camera.constants.FlashMode.auto :
+                    (this.state.flash == "on" ? Camera.constants.FlashMode.on : Camera.constants.FlashMode.off)}>
+
+          <View style={styles.toolbar}>
+              <Text style={styles.toolbarSide} onPress={() => this.handleFlashChange()}> {flashText} </Text>
+              <Text style={styles.toolbarMiddle}>ME</Text>
+              <Text style={styles.toolbarSide} onPress={() => this.handleCameraChange()}> {cameraText} </Text>
+          </View>
+          <Text
+            style={styles.capture}
+            onPress={this.takePicture.bind(this)}
+          >
+            CAPTURE
+          </Text>
         </Camera>
       </View>
     )
@@ -35,9 +65,24 @@ const styles = StyleSheet.create({
   container: {
     flex: 1
   },
+    toolbar:{
+      backgroundColor:'rgba(0,0,0,0)',
+      paddingTop:15,
+      paddingBottom:10,
+      flexDirection:'row'
+  },
+  toolbarSide:{
+      width: 50,
+      color:'#fff',
+      textAlign:'center'
+  },
+  toolbarMiddle:{
+      color:'#fff',
+      textAlign:'center',
+      flex:1
+  },
   preview: {
     flex: 1,
-    justifyContent: 'flex-end',
     alignItems: 'center',
     height: Dimensions.get('window').height,
     width: Dimensions.get('window').width
