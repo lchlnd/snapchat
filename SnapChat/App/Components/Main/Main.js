fc2ce5fbf5fd4e127a4fe84bcb2b4bcbdd4e3c80
Lachlan Dee <dee.lachlan@gmail.com>
2016-09-05 21:16:12 +1000

    Add comments/

--- SnapChat/App/Components/Main/Main.js
+++ SnapChat/App/Components/Main/Main.js
@@ -2,12 +2,14 @@ import React, { Component } from 'react'
 import { Text, View, Dimensions, StyleSheet, TouchableHighlight } from 'react-native'
 import Camera from 'react-native-camera'
 
+// Constant value to cycle flash modes.
+var FLASH_LOOKUP = {"auto":"on", "on":"off", "off":"auto"}
+
 // The root component container of the app that gets rendered after user has logged in
 class Main extends Component {
   constructor (props) {
     super()
     this.state = {
-      showProgress: false,
       frontCamera: false,
       flash: 'auto'
     }
@@ -15,29 +17,43 @@ class Main extends Component {
     this.handleFlashChange = this.handleFlashChange.bind(this);
   }
 
+  // Handle swaps between front and back camera.
   handleCameraChange() {
     this.setState({ frontCamera: !this.state.frontCamera})
   }
 
+
+  // Handle swaps between flash modes.
   handleFlashChange() {
-    var lookup = {"auto":"on", "on":"off", "off":"auto"}
-    this.setState({ flash: lookup[this.state.flash]})
+    this.setState({ flash: FLASH_LOOKUP[this.state.flash]})
+  }
+
+  // Saves user photo to the camera roll.
+  takePicture () {
+    this.camera.capture()
+    .then((data) => console.log(data))
+    .catch(err => console.log(err))
   }
 
   render () {
+    // Updates to reflect user changes to the toggle buttons.
     const cameraText = this.state.frontCamera ? 'front' : 'back';
     const flashText = this.state.flash
+
+
     return (
+
       <View style={styles.container}>
-        <Camera ref={(cam) => {
-          this.camera = cam
-        }}
+
+        {/* Main view must be camera to allow for it to take up entire screen. */}
+        <Camera ref={(cam) => {this.camera = cam }}
           style={styles.preview}
           aspect={Camera.constants.Aspect.fill}
           type={this.state.frontCamera ? Camera.constants.Type.front : Camera.constants.Type.back}
           flashMode={this.state.flash == "auto" ? Camera.constants.FlashMode.auto :
                     (this.state.flash == "on" ? Camera.constants.FlashMode.on : Camera.constants.FlashMode.off)}>
 
+          {/* Header */}
           <View style={styles.header}>
               <TouchableHighlight onPress={() => this.handleFlashChange()}>
                <Text style={styles.headerButton} > {flashText} </Text>
@@ -49,10 +65,16 @@ class Main extends Component {
                 <Text style={styles.headerButton} > {cameraText} </Text>
               </TouchableHighlight>
           </View>
+
+          {/* Footer, including capture */}
           <View style={styles.footer}>
+
+            {/* Capture */}
             <TouchableHighlight onPress={this.takePicture.bind(this)}>
               <Text style={styles.capture}> CAPTURE </Text>
             </TouchableHighlight>
+
+            {/* Button navigation */}
             <View style={styles.header}>
               <TouchableHighlight>
                <Text style={styles.headerButton} > / S / </Text>
@@ -63,17 +85,11 @@ class Main extends Component {
               <TouchableHighlight>
                 <Text style={styles.headerButton} > / St / </Text>
               </TouchableHighlight>
-              </View>
+            </View>
           </View>
         </Camera>
       </View>
     )
-  }
-
-  takePicture () {
-    this.camera.capture()
-    .then((data) => console.log(data))
-    .catch(err => console.log(err))
   }
 }
 
